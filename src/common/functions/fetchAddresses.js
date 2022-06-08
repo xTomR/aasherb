bdcbe5bc05c71b509e7f46345d23ab14c70d691a
xTomR <96185908+xTomR@users.noreply.github.com>
2022-06-08 16:12:20 -0400

    Update fetchAddresses.js

--- src/common/functions/fetchAddresses.js
+++ src/common/functions/fetchAddresses.js
@@ -1,25 +1,21 @@
 import axios from "axios";
 
-<<<<<<< Updated upstream
 const fetchinfo = async () => {
-  await axios.get(`https://api.aasherb.com/api/fetchinfo`).then();
-=======
-<<<<<<< HEAD
-const fetchAddresses = async () => {
-  const addresses = await axios.get(`${process.env.API_URL}api/addresses`).then(
-    (result) => {
-      return result;
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
-  return addresses;
-=======
-const fetchinfo = async () => {
-  await axios.get(`https://api.aasherb.com/api/fetchinfo`).then();
->>>>>>> 4b8e4bb881fc7bb3a723e575b373c892d7a18a30
->>>>>>> Stashed changes
+  await axios.get(`${process.env.API_URL}api/fetchinfo`).then();
+
+  const fetchAddresses = async () => {
+    const addresses = await axios
+      .get(`${process.env.API_URL}api/addresses`)
+      .then(
+        (result) => {
+          return result;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    return addresses;
+  };
 };
 
 export default fetchinfo;
